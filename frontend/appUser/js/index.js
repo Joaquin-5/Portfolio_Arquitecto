2211e87b8b8c2fd31e61aa005d465107e53eae87
Joaquin <joaquin.butera@gmail.com>
2023-08-19 19:38:54 -0300

    Dinamic implementations

--- frontend/appUser/js/index.js
+++ frontend/appUser/js/index.js
@@ -1,65 +1,142 @@
-const tab = document.querySelector("title");
+document.addEventListener("DOMContentLoaded", () => {
+  // Dinamic tab
+  const head = document.querySelector("head");
+  const title = document.createElement("title");
+  head.appendChild(title);
 
-const imageCarrouselContainer =
-  document.querySelector("div.container").childElementCount;
+  // Length of amount of images
+  const imageCarrouselContainer =
+    document.querySelector("div.container").childElementCount;
 
-const imageSlider = () => {
-  let counter = 1;
+  const projectsContainer = document.querySelector("article.project-container");
 
-  setInterval(() => {
-    document.querySelector(".img.show").classList.remove("show");
-    document.URL.includes("index.html") &&
-      document
-        .querySelector(".description-project.show-description-project")
-        .classList.remove("show-description-project");
+  // FAKE API
+  const url = "http://127.0.0.1:5500/frontend/projects.json";
 
-    const img = document.querySelector(`.img-${counter}`);
-    const descriptionImg =
+  const imageSlider = () => {
+    let counter = 1;
+
+    setInterval(() => {
+      document.querySelector(".img.show").classList.remove("show");
       document.URL.includes("index.html") &&
-      document.querySelector(`.description-project-${counter}`);
+        document
+          .querySelector(".description-project.show-description-project")
+          .classList.remove("show-description-project");
 
-    img.classList.add("show");
-    document.URL.includes("index.html") &&
-      descriptionImg.classList.add("show-description-project");
-    counter++;
+      const img = document.querySelector(`.img-${counter}`);
+      const descriptionImg =
+        document.URL.includes("index.html") &&
+        document.querySelector(`.description-project-${counter}`);
 
-    if (counter > imageCarrouselContainer) {
-      counter = 1;
-    }
-  }, 5000);
-};
+      img.classList.add("show");
+      document.URL.includes("index.html") &&
+        descriptionImg.classList.add("show-description-project");
+      counter++;
 
-imageSlider();
+      if (counter > imageCarrouselContainer) {
+        counter = 1;
+      }
+    }, 5000);
+  };
 
-if (document.URL.includes("index.html")) {
-  tab.innerHTML = "BFA - Fabián Butera Arquitecto";
+  imageSlider();
 
-  //h1 animation text
+  const getProject = (key) => {
+    window.location.href = `project.html?key=${key}`;
+  };
 
-  /* const h1 = new SplitType("#name-text");
+  if (document.URL.includes("index.html")) {
+    title.textContent = "BFA - Fabián Butera Arquitecto";
 
-  gsap.to(".char", {
-    y: 0,
-    stagger: 0.05,
-    delay: 0.2,
-    duration: 0.1,
-  }); */
+    //h1 animation text
 
-  // nav scroll
-  window.addEventListener("scroll", () => {
-    let nav = document.querySelector("nav");
-    nav.classList.toggle("sticky", window.scrollY > 0);
-  });
-}
+    /* const h1 = new SplitType("#name-text");
+  
+    gsap.to(".char", {
+      y: 0,
+      stagger: 0.05,
+      delay: 0.2,
+      duration: 0.1,
+    }); */
 
-if (document.URL.includes("project.html")) {
-  tab.innerHTML = "Nombre del proyecto - BFA";
-}
+    // nav scroll
+    window.addEventListener("scroll", () => {
+      let nav = document.querySelector("nav");
+      nav.classList.toggle("sticky", window.scrollY > 0);
+    });
 
-// date change footer
-const dateFooterChange = document.getElementById("date__footer-copyright");
+    const getProjects = (url) => {
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          data.map((project, id) => {
+            projectsContainer.innerHTML += `<section class="project" key=${id}>
+            <a href="./project.html">
+              <img src="./img/landscape2.avif" alt="" />
+              <div class="project__content-container">
+                <div class="project-data">
+                  <p>${project.fecha}</p>
+                  <p class="icon-text">
+                    <span class="material-symbols-outlined"> location_on </span
+                    >${project.lugar}
+                  </p>
+                </div>
+                <h3>${project.titulo}</h3>
+              </div>
+            </a>
+          </section>`;
+          });
+        })
+        .catch((error) => {
+          console.error("Error al cargar el archivo JSON:", error);
+        });
+    };
 
-let date = new Date();
-let year = date.getFullYear();
+    getProjects(url);
 
-dateFooterChange.innerHTML = year;
+    projectsContainer.addEventListener("click", function (event) {
+      event.preventDefault();
+      const clickedSection = event.target.closest("section");
+
+      if (clickedSection) {
+        const key = clickedSection.getAttribute("key");
+        console.log("La clave es: " + key);
+        getProject(key);
+      }
+    });
+
+    /* if (projectsContainer.childElementCount < 1) {
+      projectsContainer.innerHTML += `<h2 class="no-projects__title">¡No hay proyectos publicados todavía!</h2>`;
+    } */
+  }
+
+  if (document.URL.includes("project.html")) {
+    const urlParams = new URLSearchParams(window.location.search);
+    const key = urlParams.get("key");
+
+    const projectTitle = document.querySelector("h1#project-name");
+    const placeProject = document.querySelector("h2.icon-text");
+    const dateProject = document.querySelector("h2.date-project");
+    const descriptionProject = document.querySelector("p");
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data[key]);
+        title.textContent = `${data[key].titulo} - BFA`;
+        projectTitle.textContent = `${data[key].titulo}`;
+        placeProject.textContent = `${data[key].lugar}`;
+        dateProject.textContent = `${data[key].fecha}`;
+        descriptionProject.textContent = `${data[key].descripcion_larga}`;
+      });
+  }
+
+  // date change footer
+  const dateFooterChange = document.getElementById("date__footer-copyright");
+
+  let date = new Date();
+  let year = date.getFullYear();
+
+  dateFooterChange.innerHTML = year;
+});
